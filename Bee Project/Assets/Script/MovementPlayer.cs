d9b7c79b96ebd9135ce5bde6a9362827807c9e84
claudioabrun <78451778+claudioabrun@users.noreply.github.com>
2025-09-04 14:38:28 -0500

    Finished Movement Script
    Basic movement script, might change to add other features later

--- Bee Project/Assets/Script/MovementPlayer.cs
+++ Bee Project/Assets/Script/MovementPlayer.cs
@@ -7,20 +7,29 @@ public class MovementPlayer : MonoBehaviour
     public float moveSpeed;
     public Rigidbody2D rb;
 
+    private Vector2 moveDirection;
+
 
     void Update()
     {
-        
+        ProcessInputs();
     }
 
     private void FixedUpdate()
     {
-        
+        Move();
     }
 
     void ProcessInputs()
     {
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
     }
 }
